38085db27014265a0770365a0fbb022fc602b9da
Jesse Stone <jstone@psi.edu>
2022-01-26 11:02:38 -0700

    update the file size and record count of the sip label

---
+++ sip-operations/label.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import argparse
+from importlib.metadata import files
+import sys
+import os.path
+import tempfile
+import shutil
+import re
+import functools
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sip", required=True)
+    parser.add_argument("--label", required=True)
+    args = parser.parse_args()
+
+    label = readfile(args.label)
+    filesize, linecount = get_stats(args.sip)
+    replacements = {
+        "file_size": filesize,
+        "object_length": filesize,
+        "records": linecount
+    }
+    funcs = [functools.partial(replace_element, k, v) for (k,v) in replacements.items()]
+    newlabel = functools.reduce(lambda x,f: f(x), funcs, label)
+    replacefile(args.label, newlabel)
+
+    return 0
+
+def replace_element(element_name, value, str):
+    pattern = f"<{element_name}(.*)>.*</{element_name}>"
+    print (pattern)
+    replacement = f"<{element_name}\\1>{value}</{element_name}>"
+    print (replacement)
+    return re.sub(pattern, replacement, str)
+
+def readfile(filename):
+    with open(filename) as f:
+        return f.read()
+
+def replacefile(filename, contents):
+    _, output_path = tempfile.mkstemp()
+    with open(output_path, "w") as out:
+        out.write(contents)
+    
+    if os.path.exists(filename):
+        shutil.move(filename, filename + ".bak")
+    shutil.move(output_path, filename)
+
+def get_stats(filepath):
+    filesize = os.path.getsize(filepath)
+    linecount = sum(1 for x in open(filepath))
+    return filesize, linecount
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
