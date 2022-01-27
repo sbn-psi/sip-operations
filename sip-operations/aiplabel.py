f3d7cdebde82312fd6f386091e2511ff002fa488
Jesse Stone <jstone@psi.edu>
2022-01-27 16:14:09 -0700

    removed string hacking code

--- sip-operations/aiplabel.py
+++ sip-operations/aiplabel.py
@@ -54,26 +54,6 @@ def main():
 
     return 0
 
-def replace_element(element_name, value, str):
-    pattern = f"<{element_name}(.*)>.*</{element_name}>"
-    print (pattern)
-    replacement = f"<{element_name}\\1>{value}</{element_name}>"
-    print (replacement)
-    return re.sub(pattern, replacement, str)
-
-def readfile(filename):
-    with open(filename) as f:
-        return f.read()
-
-def replacefile(filename, contents):
-    _, output_path = tempfile.mkstemp()
-    with open(output_path, "w") as out:
-        out.write(contents)
-    
-    if os.path.exists(filename):
-        shutil.move(filename, filename + ".bak")
-    shutil.move(output_path, filename)
-
 def get_stats(filepath):
     filesize = os.path.getsize(filepath)
     linecount = sum(1 for x in open(filepath))
