bc1b7798e2f21032d932e4fb6b9cd6fb0fdf47fc
Jesse Stone <jstone@psi.edu>
2022-09-22 10:15:44 -0700

    updated regexes to match only end of string

--- sip-operations/deltas.py
+++ sip-operations/deltas.py
@@ -32,22 +32,22 @@ def main():
     bundle_lidvid = extract_lidvid(args.bundle_label)
     bundle_url="https://sbnarchive.psi.edu/pds4/surveys/gbo.ast.catalina.survey/bundle_gbo.ast.catalina.survey_v1.0.xml"
 
-    old_sips = find_files(args.old_dir, ".*sip.*tab")
-    sip = find_files(args.new_dir, ".*sip.*tab")[0]
+    old_sips = find_files(args.old_dir, ".*sip.*tab$")
+    sip = find_files(args.new_dir, ".*sip.*tab$")[0]
     delta_sip = sipdelta.generate_delta(old_sips, sip, dest, suffix, bundle_url)
 
-    old_transfers = find_files(args.old_dir, ".*transfer.*tab")
-    transfer = find_files(args.new_dir, ".*transfer.*tab")[0]
+    old_transfers = find_files(args.old_dir, ".*transfer.*tab$")
+    transfer = find_files(args.new_dir, ".*transfer.*tab$")[0]
     delta_transfer = transferdelta.generate_transfer_delta(transfer, old_transfers, dest, suffix, bundle_lidvid)
 
-    old_checksums = find_files(args.old_dir, ".*checksum.*tab")
-    checksum = find_files(args.new_dir, ".*checksum.*tab")[0]
+    old_checksums = find_files(args.old_dir, ".*checksum.*tab$")
+    checksum = find_files(args.new_dir, ".*checksum.*tab$")[0]
     delta_checksum = checksumdelta.generate_checksum_delta(checksum, old_checksums, dest, suffix, args.bundle_label)
 
-    aip_label = find_files(args.new_dir, ".*aip.*xml")[0]
+    aip_label = find_files(args.new_dir, ".*aip.*xml$")[0]
     aip_lidvid, generated_aip_label = aiplabel.gen_aip_label(delta_checksum, delta_transfer, aip_label, dest, suffix)
 
-    sip_label = find_files(args.new_dir, ".*sip.*xml")[0]
+    sip_label = find_files(args.new_dir, ".*sip.*xml$")[0]
     siplabel.gen_sip_label(delta_sip, sip_label, generated_aip_label, aip_lidvid, dest, suffix)
 
     return 0
