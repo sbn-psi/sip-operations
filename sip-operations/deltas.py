98e80a04a24ec1e8a1c6178c89b94f4d5beb2806
Jesse Stone <jstone@psi.edu>
2023-06-12 11:49:47 -0700

    extracted generate_checksum_delta, generate_aip_delta, gen_sip_delta

--- sip-operations/deltas.py
+++ sip-operations/deltas.py
@@ -37,26 +37,32 @@ def generate_deltas(old_dir, new_dir, bundle_label, bundle_url="https://sbnarchi
 
     transfer = find_files(new_dir, ".*transfer.*tab$")[0]
     superseded_collection_lidvids = transferdelta.get_superseded_collection_lidvids(transfer)
-    
-    
+
+    delta_checksum = generate_checksum_delta(transfer, superseded_collection_lidvids, old_dir, new_dir, dest, suffix, bundle_label)
+    aip_lidvid, generated_aip_label = generate_aip_delta(bundle_label, transfer, dest, suffix, superseded_collection_lidvids, old_dir, new_dir, delta_checksum)
+    delta_sip = gen_sip_delta(old_dir, new_dir, dest, suffix, bundle_url, superseded_collection_lidvids)
+
+    sip_label = find_files(new_dir, ".*sip.*xml$")[0]
+    siplabel.gen_sip_label(delta_sip, sip_label, generated_aip_label, aip_lidvid, dest, suffix)
+
+
+def generate_checksum_delta(transfer, superseded_collection_lidvids, old_dir, new_dir, dest, suffix, bundle_label):
     superseded_collection_filenames = transferdelta.get_superseded_collection_filenames(transfer, superseded_collection_lidvids)
     old_checksums = find_files(old_dir, ".*checksum.*tab$")
     checksum = find_files(new_dir, ".*checksum.*tab$")[0]
-    delta_checksum = checksumdelta.generate_checksum_delta(checksum, old_checksums, dest, suffix, bundle_label, superseded_collection_filenames)
+    return checksumdelta.generate_checksum_delta(checksum, old_checksums, dest, suffix, bundle_label, superseded_collection_filenames)
 
-
+def generate_aip_delta(bundle_label, transfer, dest, suffix, superseded_collection_lidvids, old_dir, new_dir, delta_checksum):
     bundle_lidvid = extract_lidvid(bundle_label)
     old_transfers = find_files(old_dir, ".*transfer.*tab$")
     delta_transfer = transferdelta.generate_transfer_delta(transfer, old_transfers, dest, suffix, bundle_lidvid, superseded_collection_lidvids)
     aip_label = find_files(new_dir, ".*aip.*xml$")[0]
-    aip_lidvid, generated_aip_label = aiplabel.gen_aip_label(delta_checksum, delta_transfer, aip_label, dest, suffix)
+    return aiplabel.gen_aip_label(delta_checksum, delta_transfer, aip_label, dest, suffix)
 
-
+def gen_sip_delta(old_dir, new_dir, dest, suffix, bundle_url, superseded_collection_lidvids):
     old_sips = find_files(old_dir, ".*sip.*tab$")
     sip = find_files(new_dir, ".*sip.*tab$")[0]
-    sip_label = find_files(new_dir, ".*sip.*xml$")[0]
-    delta_sip = sipdelta.generate_delta(old_sips, sip, dest, suffix, bundle_url, superseded_collection_lidvids)
-    siplabel.gen_sip_label(delta_sip, sip_label, generated_aip_label, aip_lidvid, dest, suffix)
+    return sipdelta.generate_delta(old_sips, sip, dest, suffix, bundle_url, superseded_collection_lidvids)
 
 
 def find_files(dirname, pattern):
